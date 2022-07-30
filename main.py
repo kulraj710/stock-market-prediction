8e70d521cf8241cc11102074d5ec774a465ad722
kulraj710 <kulraj710@gmail.com>
2022-07-30 19:42:51 +0530

    add streamlit, (saving model to reuse with h5)

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ import pandas as pd
 import pandas_datareader as web
 import datetime as dt
 import yfinance as yf
+import pickle
 
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, LSTM
-
+from keras.models import load_model
 
 # load data
 company = 'RELIANCE.NS'
@@ -58,9 +59,21 @@ model.add(Dense(units=1)) # prediction of the next price
 
 
 model.compile(optimizer='adam', loss='mean_squared_error')
-model.fit(x_train, y_train, batch_size=32, epochs=25)
+model.fit(x_train, y_train, batch_size=10, epochs=5)
+
+
+
+# saving the model using pickle
+
+# saving_model = pickle.dumps(model)
+model.save('testmodel.h5')
+
 
 ''' Test the Model Accuracy on Existing Data'''
+
+# loading the saved model!
+
+saved_model = load_model('testmodel.h5')
 
 # load Test Data
 
@@ -89,7 +102,7 @@ for x in range(predictions_days, len(model_input)):
 x_test = np.array(x_test)
 x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
 
-predicted_prices = model.predict(x_test)
+predicted_prices = saved_model.predict(x_test)
 predicted_prices = scaler.inverse_transform(predicted_prices)
 
 
@@ -110,7 +123,7 @@ real_data = [model_input[len(model_input) + 1 - predictions_days:len(model_input
 real_data = np.array(real_data)
 real_data = np.reshape(real_data, (real_data.shape[0], real_data.shape[1], 1))
 
-prediction = model.predict(real_data)
+prediction = saved_model.predict(real_data)
 prediction = scaler.inverse_transform(prediction)
 
 print(f"Predicted Price of {company} on {test_end} is {prediction[0][0]}")
